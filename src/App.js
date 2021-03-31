5bf0381f29eb6117acd84382266c51a6b1d5a324
Jennifer Stewart <jstewart8053@gmail.com>
2021-03-31 11:52:39 -0500

    Added Nav and Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./styles.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div className="App">Tester</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+    </div>
+  );
 }
